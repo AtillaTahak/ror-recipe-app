2176acb8018a67a090dbccd47b0b11a0ecd0fec8
Ricky Mormor <rickymormor1@gmail.com>
2022-04-27 15:04:17 +0100

    Add relations between user and foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,7 @@
 class Food < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
--- db/migrate/20220426170218_create_foods.rb
+++ db/migrate/20220426170218_create_foods.rb
@@ -1,6 +1,11 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
+      t.string :name
+      t.string :measurement_unit
+      t.float :price
+      t.integer :user_id
+      t.references :author, foreign_key: { to_table: :users }
 
       t.timestamps
     end
