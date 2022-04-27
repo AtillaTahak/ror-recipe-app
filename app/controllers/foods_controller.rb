8e6cfcab2a607f0e91086861c7b8aafdfd8ec227
Ricky Mormor <rickymormor1@gmail.com>
2022-04-27 16:55:22 +0100

    Fix flash duplicate message

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -23,7 +23,7 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: "Food was successfully created." }
+        format.html { redirect_to foods_url, notice: "Food was successfully created." }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
