653a530b982a1e1e41885bda3067e4b30faa4ba5
atillatahak <atillatahaa@gmail.com>
2022-04-30 10:57:15 +0300

    fixed error

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -3,7 +3,5 @@ class GeneralShoppingListController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @inventory = Inventory.find(params[:inventory_id])
     @shopping_list = @inventory.compute_missing_foods(@recipe)
-  rescue Exception => e
-    flash[:notice] = e.message
   end
 end
